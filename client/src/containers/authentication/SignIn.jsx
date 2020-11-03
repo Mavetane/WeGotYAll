b9b9422cbfd96197c69574bc1856c7725e64d5e9
Collen Maphike <collenmaphike25@gmail.com>
2020-11-03 08:28:29 +0200

    added functionality to signin and signup components

--- client/src/containers/authentication/SignIn.jsx
+++ client/src/containers/authentication/SignIn.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+
 
 export function SignIn () {
+  const [signInDetails, setSignInDetails] = useState({ email: "", password: "", errors: "", success: "" });
+  const { email, password } = signInDetails;
+
+  const handleChange = e => {
+    const { value, name } = e.target;
+    setSignInDetails({ ...signInDetails, [name]: value })
+  }
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('signInDetails', signInDetails)
+  }
+
   return (
     <div>
       <h1>SignIn</h1>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={onSubmit} >
+        <input type="email" placeholder="Email" value={email} onChange={handleChange} name="email" />
+        <input type="password" placeholder="Password" value={password} onChange={handleChange} name="password" />
+        <input type="submit" />
       </form>
     </div>
   )
--- client/src/containers/authentication/SignUp.jsx
+++ client/src/containers/authentication/SignUp.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 export function SignUp () {
+  const [signUpDetails, setSignUpDetails] = useState({ email: "", password: "", errors: "", success: "" });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setSignUpDetails({ ...signUpDetails, [name]: value })
+  }
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('signUpDetails', signUpDetails)
+  }
+  const { email, password } = signUpDetails;
   return (
     <div>
       <h1>SignUp</h1>
-      <form>
-        <input type="text" placeholder="UserName" />
-        <input type="text" placeholder="Email" />
+      <form onSubmit={onSubmit}>
+        <input type="email" placeholder="Email" onChange={handleChange} value={email} name="email" />
+        <input type="password" placeholder="Password" onChange={handleChange} value={password} name="password" />
         <input type="submit" />
       </form>
     </div>
